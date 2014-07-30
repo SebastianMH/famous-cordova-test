bcc024ddf9d355f7e8f5d391bdffa01518ec8e3e
Sebastian Miller-Hack <sebastiantheeng@gmail.com>
2014-07-30 13:25:17 -0700

    cleaned lint

--- test1/www/src/main.js
+++ test1/www/src/main.js
@@ -6,34 +6,26 @@ define(function(require, exports, module) {
     var ImageSurface = require('famous/surfaces/ImageSurface');
     var Surface = require('famous/core/Surface');
 
-    // create the main context
     var mainContext = Engine.createContext();
-    document.addEventListener("deviceready", onDeviceReady, false);
+    var initialTime = Date.now();
 
-    //http://plugins.cordova.io/#/package/org.apache.cordova.device
     function onDeviceReady() {
-        console.log('===================================================');
-        console.log(device.uuid);
-        console.log('===================================================');
-
         text.setContent(device.platform);
     }
 
-    // function rockOn() {
+    document.addEventListener('deviceready', onDeviceReady, false);
 
-    // }
-    // your app here
     var logo = new ImageSurface({
         size: [200, 200],
         content: 'http://code.famo.us/assets/famous_logo.svg',
         classes: ['double-sided']
     });
+
     var text = new Surface({
         size: [200, 200],
         content: 'none yet'
     });
 
-    var initialTime = Date.now();
     var centerSpinModifier = new Modifier({
         origin: [0.5, 0.5],
         transform: function() {
@@ -47,5 +39,4 @@ define(function(require, exports, module) {
 
     mainContext.add(textModifier).add(text);
     mainContext.add(centerSpinModifier).add(logo);
-
 });
